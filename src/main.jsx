63c5d9133b0686a85b22fd2d8245728e09efc13f
Martin Joukl <jouklma1@gmail.com>
2024-05-04 18:02:49 +0200

    Main concepts 6

--- src/main.jsx
+++ src/main.jsx
@@ -95,5 +95,44 @@ function App() {
     );
 }
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+class LoggingButton extends React.Component {
+    handleClick() {
+        console.log('this is:', this);
+    }
+
+    render() {
+        // This syntax ensures `this` is bound within handleClick
+        return (
+            <button onClick={() => this.handleClick()}>
+                Click me
+            </button>
+        );
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Toggle />);
