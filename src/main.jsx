979c0eacb93fddfdd8f9bd5cebc754572c5c0495
Martin Joukl <jouklma1@gmail.com>
2024-05-04 17:37:58 +0200

    Main concepts 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,71 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-);
-
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    root.render(element);
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
 
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
 
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
 
-setInterval(tick, 1000);
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+             src={props.user.avatarUrl}
+             alt={props.user.name}
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author} />
+);
